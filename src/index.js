dc960eb27bb9cb9af3d5dc60ce64cda8e8a3c6ef
shin <65806493+shin-ito0306@users.noreply.github.com>
2021-10-15 21:33:05 +0900

    論理演算子の本当の意味

--- src/index.js
+++ src/index.js
@@ -194,3 +194,28 @@ const namearr = ["田中", "山田", "じゃけぇ"];
 //   return num1 + num2 > 100 ? '100を超えています。' : '許容範囲内です。';
 // }
 // console.log(checknum(50, 30));
+
+/**
+ * 論理演算子の本当の意味
+ */
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueです")
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります")
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee)
+
+// &&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
